fbbd95c061dcc2d284590d43dc77a0388d24d931
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-04 13:08:28 +0000

    refactor: change script in package.json add a test table for testing

--- db/schema.ts
+++ db/schema.ts
@@ -3,5 +3,11 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
+  username: text("username").notNull(),
   userId: text("user_id").notNull(),
+});
+
+export const update = pgTable("update", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
 });
