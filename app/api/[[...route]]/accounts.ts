f8db4cf1e4a71f954df8fb4a99c723044093ed26
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-07 21:30:36 +0000

    feat: add POST /accounts endpoint with clerk auth and validation.
    - Protects route with clerk middleware
    - Validates incoming JSON using accountInsertSchema
    - Insert new account tied to authenticated user
    - Returns inserted data as response

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,23 +1,52 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accountInsertSchema, accounts } from "@/db/schema";
+import { createId } from "@paralleldrive/cuid2";
 
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
 
-  if (!auth?.userId) {
-    return c.json({ error: "unauthorized" }, 401);
-  }
+    if (!auth?.userId) {
+      return c.json({ error: "unauthorized" }, 401);
+    }
 
-  const data = await db
-    .select({ id: accounts.id, name: accounts.name })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId));
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-  return c.json({ data });
-});
+    return c.json({ data });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", accountInsertSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+
+      return c.json({
+        data,
+      });
+    }
+  );
 
 export default app;
--- db/schema.ts
+++ db/schema.ts
@@ -1,13 +1,11 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
   name: text("name").notNull(),
-  username: text("username").notNull(),
   userId: text("user_id").notNull(),
 });
 
-export const update = pgTable("update", {
-  id: text("id").primaryKey(),
-  name: text("name").notNull(),
-});
+export const accountInsertSchema = createInsertSchema(accounts);
