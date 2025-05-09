41cbfd37dc381697205e1ce2e3656c1fce334685
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-09 12:01:59 +0000

    feat: integrate AccountForm into NewAccountSheet with mutation handling

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import {
@@ -8,9 +9,30 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useNewAccount } from "../hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { useCreateAccount } from "../api/use-create-account";
+import { accountInsertSchema } from "@/db/schema";
+import { z } from "zod";
+
+const formSchema = accountInsertSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
@@ -20,6 +42,7 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm onSubmit={onSubmit} disabled={mutation.isPending} />
       </SheetContent>
     </Sheet>
   );
