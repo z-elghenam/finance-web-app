8ca4a20d2589db702c5c2b97f3526a29dbe86ece
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-18 11:52:52 +0000

    feat: add categories and transactions tables with foreign key constraints

---
+++ drizzle/0005_faithful_lenny_balinger.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "transactions" (
+	"id" text PRIMARY KEY NOT NULL,
+	"amount" integer NOT NULL,
+	"payee" text NOT NULL,
+	"notes" text,
+	"date" timestamp NOT NULL,
+	"account_id" text NOT NULL,
+	"category_id" text
+);
+--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_account_id_accounts_id_fk" FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE set null ON UPDATE no action;
