52f85ae43a27528eedc06b121ca40ba37ab99352
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-27 11:54:57 +0000

    feat: update DashboardPage to include DataGrid and DataCharts components

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <div>dashboard page</div>;
-};
+import { DataGrid } from "@/components/data-grid";
+import { DataCharts } from "@/components/data-charts";
 
-export default Home;
+export default function DashboardPage() {
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
+      <DataCharts />
+    </div>
+  );
+};
