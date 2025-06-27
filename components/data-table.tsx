0ccabf2429ec3e62a90edc6d3d06a57a1e705220
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-27 11:55:23 +0000

    feat: add CountUp component and update DataTable to use dynamic filter key

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -75,9 +75,9 @@ export function DataTable<TData, TValue>({
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
+            table.getColumn(filterKey)?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
